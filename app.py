c64f32d06bddbf60fb83960353175cf344758bef
Chris <info@chrisgkalfas.com>
2019-01-23 16:40:20 +0200

    update
    update

---
+++ app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import Flask
+from flask_restful import Resource, Api
+from Search import Search
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Index(Resource):
+    def get(self):
+        return {"data": "hello world"}, 200, {'Access-Control-Allow-Origin': '*'}
+
+
+api.add_resource(Index, '/')
+api.add_resource(Search, '/search/<string:search_term>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
