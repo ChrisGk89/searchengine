c905a7f48422a1aec2ad1ab6aced68b116c7be98
Chris <info@chrisgkalfas.com>
2019-01-23 16:40:59 +0200

    update
    update

---
+++ Page.py
@@ -0,0 +1,10 @@
+# URL of the page
+# pageRank score
+# words in the page
+# links in the page
+class Page():
+    def __init__(self):
+        self.url = ""
+        self.pageRank = 1.0
+        self.words = []
+        self.links = []
